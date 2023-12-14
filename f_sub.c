2b7564df76fcde3573880b163853a083fd0009e2
Meron Edea <edeameron@gmail.com>
2023-12-14 06:01:21 -0900

    sub

---
+++ f_sub.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+void sub(int *stack, int *top) {
+    if (*top < 2) {
+        printf("Error: can't sub, stack too short\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int top_element = stack[*top - 1];
+    int second_top_element = stack[*top - 2];
+
+    stack[*top - 2] = second_top_element - top_element;
+    (*top)--;
+
+    printf("Stack: ");
+    for (int i = 0; i < *top; i++) {
+        printf("%d ", stack[i]);
+    }
+    printf("\n");
+}
--- monty.h
+++ monty.h
@@ -60,5 +60,6 @@ void pop(stack_t **top, unsigned int line_number);
 void swap(stack_t **top, unsigned int line_number);
 void add(stack_t **top, unsigned int line_number);
 void nop(stack_t **top, unsigned int line_number);
+void sub(int *stack, int *top);
 
 #endif
